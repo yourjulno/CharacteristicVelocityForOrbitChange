478d25161b5b8cb06926d8f024d89cea8c06e6e9
yourjulno <diugaeva.iud@phystech.edu>
2023-08-09 19:44:04 +0300

    добавлен тест для скорости как отдельной функции

--- tests/test_TransferElliptic.cpp
+++ tests/test_TransferElliptic.cpp
@@ -53,8 +53,9 @@ TEST(computeParametersForVelocity, FIRST){
 
     const std::size_t count = 500;
     const CountsForSteps counts = {count, count,
-                                   count, count, count};
-    const auto result = computeParametersForVelocity_(first, final, counts);
+                                   count};
+    const scalar trashHold = 0.01;
+    const auto result = computeParametersForVelocity_(first, final, counts, trashHold);
     std::cout << result.value().semimajorAxisTransferOrbit << " ";
     std::cout << result.value().periapseA1 << " ";
     std::cout << result.value().angleBetweenImpulseAndTransversal1 * 180 / M_PI << " ";
@@ -77,9 +78,57 @@ TEST(computeVelocity, FIRST){
 
     const std::size_t count = 500;
     const CountsForSteps counts = {count, count,
-                                   count, count, count};
-    const auto result = computeParametersForVelocity_(first, final, counts);
-    const auto deltaV = computeVelocity(first, final, result, nu);
+                                    count};
+    const scalar trashHold = 0.01;
+    const auto result = computeParametersForVelocity_(first, final, counts, trashHold);
+    const auto deltaV = computeVelocity(first, final, result);
+    const scalar realVelocity = std::sqrt(nu / semimajorAxis2);
+
+    const scalar ellipticVelocity1 = std::sqrt(nu * 2 * semimajorAxis2 /
+                                               semimajorAxis1 / (semimajorAxis1 + semimajorAxis2));
+    const scalar ellipticVelocity2 = std::sqrt(nu * 2 * semimajorAxis1 /
+                                               semimajorAxis2 / (semimajorAxis1 + semimajorAxis2));
+    const scalar velocityOnFirstrbit  = std::sqrt(nu / semimajorAxis1);
+    const scalar velocityOnSEcondOrbit = std::sqrt(nu / semimajorAxis2);
+    const scalar firstdelta = ellipticVelocity1 - velocityOnFirstrbit;
+
+    const scalar seconddelta = velocityOnSEcondOrbit - ellipticVelocity2;
+    const scalar realResultDeltaV =  firstdelta + seconddelta;
+    std::cout << deltaV  << " " << realResultDeltaV;
+}
+TEST(Velocity, FIRST){
+    const scalar nu = 3.986028e14;
+    const scalar G =  6.67430e-11;
+    const scalar semimajorAxis1 = 3;
+    const scalar semiminorAxis1 = 1;
+    const scalar semimajorAxis2 = 2;
+    const scalar semiminorAxis2 = 1;
+    const scalar ascendNode1 = 0;
+    const scalar ascendNode2 = 0;
+    const scalar anomaly1 = 115.196 * deg;
+    const scalar anomaly2 = -54.961 * deg;
+    const scalar polarEq1 = semimajorAxis1 + semiminorAxis1 * std::cos(anomaly1 - ascendNode1);
+    const scalar polarEq2 = semimajorAxis2 + semiminorAxis2 * std::cos(anomaly2 - ascendNode2);
+    const scalar angle1 = 9.594 * deg;
+    const scalar angle2 = 9.594 * deg;
+    const scalar semimajorAxisTransferOrbit = 2.49981;
+    const scalar periapse1 = semiminorAxis1 * std::sin(anomaly1 - ascendNode1) /
+            std::tan(angle1) / std::sqrt(semimajorAxis1);
+    const scalar periapse2 = semiminorAxis2 * std::sin(anomaly2 - ascendNode2) /
+                             std::tan(angle2) / std::sqrt(semimajorAxis2);
+    const CorrectParametresForTransfer params = {angle1, angle2,
+                                                 polarEq1, polarEq2,
+                                                 periapse1, periapse2,
+                                                 semimajorAxisTransferOrbit };
+    const EllipticOrbit first = {semimajorAxis1, semiminorAxis1, ascendNode1};
+    const EllipticOrbit final = {semimajorAxis2, semiminorAxis2, ascendNode2};
+
+    const std::size_t count = 500;
+    const CountsForSteps counts = {count, count,
+                                   count};
+    const scalar trashHold = 0.01;
+    //const auto result = computeParametersForVelocity_(first, final, counts, trashHold);
+    const auto deltaV = computeVelocity(first, final, params);
     const scalar realVelocity = std::sqrt(nu / semimajorAxis2);
 
     const scalar ellipticVelocity1 = std::sqrt(nu * 2 * semimajorAxis2 /
